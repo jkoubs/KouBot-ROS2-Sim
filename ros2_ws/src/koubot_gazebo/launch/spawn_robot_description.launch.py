889d1896982f1d0432ed901545b8d617647ba308
Jason Koubi <jazkoubi@gmail.com>
2024-03-13 08:41:45 -0700

    camera + liDAR added in URDF

--- ros2_ws/src/koubot_gazebo/launch/spawn_robot_description.launch.py
+++ ros2_ws/src/koubot_gazebo/launch/spawn_robot_description.launch.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
 from launch_ros.actions import Node
 from launch import LaunchDescription
-
 # Read the robot_description topic and spawn that robot mdl
 
 def generate_launch_description():
-
     position = [0.0, 0.0, 0.2]
     orientation = [0.0, 0.0, 0.0]
     robot_name = "koubot"
