0ebc0294f605c155247a7f9d759700ddc8a7ed55
Jason Koubi <jazkoubi@gmail.com>
2025-06-30 22:18:57 -0700

    add rviz node

--- ros2_ws/src/koubot_slam/launch/slam.launch.py
+++ ros2_ws/src/koubot_slam/launch/slam.launch.py
@@ -3,7 +3,8 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     slam_params_file = LaunchConfiguration('slam_params_file')
@@ -17,6 +18,9 @@ def generate_launch_description():
         description='Full path to the ROS2 parameters file to use for the slam_toolbox node',
     )
 
+    rviz_config_file = os.path.join(get_package_share_directory(
+        'koubot_slam'), 'rviz', 'slam.rviz')
+
     use_sim_time_arg = DeclareLaunchArgument(
         'use_sim_time', default_value='true', description='Use simulation/Gazebo clock'
     )
@@ -28,8 +32,18 @@ def generate_launch_description():
         parameters=[slam_params_file, {'use_sim_time': use_sim_time}],
     )
 
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        name='rviz_node',
+        parameters=[{'use_sim_time': True}],
+        arguments=['-d', rviz_config_file]
+    )
+
     return LaunchDescription([
         use_sim_time_arg,
         slam_params_file_arg,
+        rviz,
         slam_node
     ])
