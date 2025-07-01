6758f596bf79e741fa9dfa639a0eb966e1806b51
Jason Koubi <jazkoubi@gmail.com>
2025-06-30 22:17:36 -0700

    rm rviz

--- ros2_ws/src/robot_description/launch/koubot_description.launch.py
+++ ros2_ws/src/robot_description/launch/koubot_description.launch.py
@@ -75,7 +75,7 @@ def generate_launch_description():
     return LaunchDescription(
         [
             robot_state_publisher_node,
-            rviz_node,
+            #rviz_node,
             joint_state_publisher_node,
             joint_state_publisher_gui_node
         ]
